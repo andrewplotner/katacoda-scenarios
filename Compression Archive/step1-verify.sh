0fc048aa203209005cae95f4789786d3574bf235
andrewplotner <andrewplotner@yahoo.com>
2020-08-06 07:56:53 -0400

    Update step1-verify.sh

--- Compression Archive/step1-verify.sh
+++ Compression Archive/step1-verify.sh
@@ -8,6 +8,13 @@
 ################################
 #Description of function: checks if a file exists
 #Arguments: file path
+#Parameters to use with test
+#-e: Returns true value if file exists.
+#-f: Return true value if file exists and regular file.
+#-r: Return true value if file exists and is readable.
+#-w: Return true value if file exists and is writable.
+#-x: Return true value if file exists and is executable.
+#-d: Return true value if exists and is a directory.
 ################################
 function check-file(){
 
