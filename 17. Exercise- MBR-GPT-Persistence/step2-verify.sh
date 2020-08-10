13c7cb3d10fd9d669f3e56d50a2f195361a51a7a
andrewplotner <andrewplotner@yahoo.com>
2020-08-10 12:51:00 -0400

    Update step2-verify.sh

--- 17. Exercise- MBR-GPT-Persistence/step2-verify.sh
+++ 17. Exercise- MBR-GPT-Persistence/step2-verify.sh
@@ -1,1 +1,1 @@
-OUT=$( df -hP | grep vdb1 | cut -d/ -f2 ) && [[ $OUT == dev ]] && IN=$( df -hP | grep vdb2 | cut -d/ -f2 ) && [[ $IN == dev ]] && echo done
+OUT=$( df -hP | grep vdb1 | cut -d/ -f2 ) && [[ $OUT -eq dev ]] && IN=$( df -hP | grep vdb2 | cut -d/ -f2 ) && [[ $IN -eq dev ]] && echo done
