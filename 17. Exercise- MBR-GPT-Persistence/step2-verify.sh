6054dae44e1c68e1aee28bb6fed81c6380bd5e20
andrewplotner <andrewplotner@yahoo.com>
2020-08-10 12:59:51 -0400

    Update step2-verify.sh

--- 17. Exercise- MBR-GPT-Persistence/step2-verify.sh
+++ 17. Exercise- MBR-GPT-Persistence/step2-verify.sh
@@ -1,1 +1,1 @@
-OUT=$( df -hP | grep vdb1 | cut -d/ -f2 ) && [[ $OUT -eq dev ]] && IN=$( df -hP | grep vdb2 | cut -d/ -f2 ) && [[ $IN -eq dev ]] && echo done
+[ -e /mnt/mymount1/lost+found ] && [ -e /mnt/mymount2/lost+found ] && echo done
