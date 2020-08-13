473eaccb1f61dfe5884e9abd5cd6014be55cc782
andrewplotner <andrewplotner@yahoo.com>
2020-08-13 09:54:39 -0400

    Update startup.sh

--- 24. Skill Test 1- ssh,umask, permissions/assets/startup.sh
+++ 24. Skill Test 1- ssh,umask, permissions/assets/startup.sh
@@ -2,11 +2,9 @@
 
 
 touch {a,b,c,check.sh} ;
-
 echo '#!/bin/bash' > check.sh ;
 echo "bash /tmp/deploy.sh" >> check.sh ;
-
 GREEN=`tput setaf 2`; RESET=`tput sgr0`;
-
 tar -zcf deploy_content.tar.gz a b c check.sh 
-ssh node01 useradd -m mary -p password ;
+
+ssh node01 useradd -m mary -p password 
