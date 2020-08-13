cdfc86944cb3b2da6d02d2439c47c6dcdb35acb1
andrewplotner <andrewplotner@yahoo.com>
2020-08-13 09:01:21 -0400

    Create startup.sh

---
+++ 24. Skill Test 1- ssh,umask, permissions/assets/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+(cd /root ;
+
+touch {a,b,c,check.sh} ;
+
+echo '#!/bin/bash' > check.sh ;
+echo "bash /tmp/deploy.sh" >> check.sh ;
+
+useradd -m mary -p password ;
+
+GREEN=`tput setaf 2`; RESET=`tput sgr0`;
+
+tar -zcf deploy_content.tar.gz a b c check.sh ;)
